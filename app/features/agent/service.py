e4a598777545bca98e0b40a3b350924a79c3bbbd
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2025-10-13 14:11:12 +0500

    feat: greet_service, sent_email_service, perform_action_service and chat_handler created.

--- app/features/agent/service.py
+++ app/features/agent/service.py
@@ -1,10 +1,28 @@
 from langchain_core.output_parsers import PydanticOutputParser
 from .chat import generate_response
-from .schemas import AnalyzeMessageSchema
-from .templates import ANALYZE_MSG_TEMPLATE
+from .schemas import AnalyzeMessageSchema, SentEmailSchema, GreetMessageSchema
+from .templates import ANALYZE_MSG_TEMPLATE, GREETING_TEMPLATE, SENT_EMAIL_TEMPLATE
 
 
 async def analyze_message_service(query: str) -> AnalyzeMessageSchema:
     parser = PydanticOutputParser(pydantic_object=AnalyzeMessageSchema)
     return await generate_response(ANALYZE_MSG_TEMPLATE, parser, query)
-    
+
+async def greet_service(query: str) -> AnalyzeMessageSchema:
+    parser = PydanticOutputParser(pydantic_object=GreetMessageSchema)
+    return await generate_response(GREETING_TEMPLATE, parser, query)
+
+async def sent_email_service(query: str) -> SentEmailSchema:
+    parser = PydanticOutputParser(pydantic_object=SentEmailSchema)
+    return await generate_response(SENT_EMAIL_TEMPLATE, parser, query)
+
+async def perform_action_service(query: str, action: str):
+    if action == "greet":
+        return await greet_service(query)
+    elif action == "send_email":
+        return await sent_email_service(query)
+
+
+async def chat_handler(query: str):
+    action = await analyze_message_service(query)
+    return await perform_action_service(query, action.tool)
